c6dd043090da00f3c94398e1c66eb119cc5d3cbe
zhangyu <yuzhangsir@gmail.com>
2021-12-08 20:24:38 +0800

    冒泡排序：鸡尾酒排序（钟锤）

--- edtion1/chapter4/chapter4_test.go
+++ edtion1/chapter4/chapter4_test.go
@@ -12,3 +12,9 @@ func TestBubbleSort(t *testing.T)  {
 	part2.BubbleSort(arr)
 	fmt.Println(arr)
 }
+
+func TestCockTailSort(t *testing.T)  {
+	arr := []int{2,3,4,5,6,7,8,1}
+	part2.CockTailSort(arr)
+	fmt.Println(arr)
+}
--- edtion1/chapter4/part2/cocktailsort.go
+++ edtion1/chapter4/part2/cocktailsort.go
@@ -1,1 +1,32 @@
 package part2
+
+//冒泡排序：鸡尾酒排序（钟锤）
+
+func CockTailSort(arr []int) {
+	// 最外层len(arr)/2
+	for i := 0; i < len(arr)/2; i++ {
+		//奇数轮：钟锤从左往右走
+		//有序标记
+		isSorted := true
+		for j := i; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				isSorted = false
+			}
+		}
+		if isSorted {
+			break
+		}
+		//偶数轮：钟锤从右往左走
+		isSorted = true
+		for j := len(arr) - 1 - i; j > i; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+				isSorted = false
+			}
+		}
+		if isSorted {
+			break
+		}
+	}
+}
