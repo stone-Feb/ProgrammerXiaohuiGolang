4fb0bc301f5188edeaf52600430a2a020eb77cb6
zhangyu <yuzhangsir@gmail.com>
2021-12-23 20:48:48 +0800

    BucketSort

--- edtion1/chapter4/chapter4_test.go
+++ edtion1/chapter4/chapter4_test.go
@@ -10,42 +10,47 @@ import (
 )
 
 //记录有序边界的方式排序
-func TestBubbleSort(t *testing.T)  {
-	arr := []int{3,4,2,1,5,6,7,8}
+func TestBubbleSort(t *testing.T) {
+	arr := []int{3, 4, 2, 1, 5, 6, 7, 8}
 	part2.BubbleSort(arr)
 	fmt.Println(arr)
 }
 
 //鸡尾酒排序
-func TestCockTailSort(t *testing.T)  {
-	arr := []int{2,3,4,5,6,7,8,1}
+func TestCockTailSort(t *testing.T) {
+	arr := []int{2, 3, 4, 5, 6, 7, 8, 1}
 	part2.CockTailSort(arr)
 	fmt.Println(arr)
 }
 
-func TestStr(t *testing.T)  {
+func TestStr(t *testing.T) {
 	str := "oJQRhxPwOAYkup8cAd-Vjg0htVJU"
 	fmt.Println(len(str))
 }
 
-func TestQuickSort(t *testing.T)  {
-	arr := []int{4,4,6,5,3,2,8,1}
+func TestQuickSort(t *testing.T) {
+	arr := []int{4, 4, 6, 5, 3, 2, 8, 1}
 	//arr := []int{4,7,6,5,3,2,8,1}
 	//part3.QuickSort(arr , 0 , len(arr)-1)
-	part3.QuickSortWithStack(arr , 0 , len(arr)-1)
+	part3.QuickSortWithStack(arr, 0, len(arr)-1)
 	fmt.Println(arr)
 }
 
-func TestHeapSort(t *testing.T)  {
-	arr := []int{4,4,6,5,3,2,8,1}
+func TestHeapSort(t *testing.T) {
+	arr := []int{4, 4, 6, 5, 3, 2, 8, 1}
 	part4.HeapSort(arr)
 	fmt.Println(arr)
 }
 
-func TestCountSort(t *testing.T)  {
+func TestCountSort(t *testing.T) {
 	//arr := []int{4,4,6,5,3,2,8,1,7,5,6,0,10}
 	//part5.CountSort(arr)
 
-	arr2 := []int{95,94,91,98,99,90,99,93,91,92}
+	arr2 := []int{95, 94, 91, 98, 99, 90, 99, 93, 91, 92}
 	part5.CountSortV2(arr2)
+}
+
+func TestBucketSort(t *testing.T) {
+	arr := []float64{4.12,6.421,0.0023,3.0,2.123,8.122,4.12, 10.09}
+	part5.BucketSort(arr)
 }
--- edtion1/chapter4/part5/bucketsort.go
+++ edtion1/chapter4/part5/bucketsort.go
@@ -1,3 +1,65 @@
 package part5
 
-//桶排序
+import (
+	"container/heap"
+	"fmt"
+	"math"
+)
+
+//BucketSort 桶排序
+func BucketSort(arr []float64) {
+	//1. 得到数列的最大值和最小值，并算出差值
+	max, min := arr[0], arr[0]
+	for _, v := range arr {
+		max = math.Max(max, v)
+		min = math.Min(min, v)
+	}
+	d := max - min
+	//2. 初始化桶
+	bucketNum := len(arr)
+	bucketList := make([]float64Heap, bucketNum)
+	for i := 0; i < bucketNum; i++ {
+		bucketList = append(bucketList, float64Heap{})
+	}
+	//3.遍历原是数组，将每个元素放入桶中
+	for _, v := range arr {
+		num := int((v - min) * (float64(bucketNum) - 1) / d)
+		//bucketList[num].Push(v)
+		//最小堆实现
+		heap.Push(&bucketList[num],v)
+	}
+	//4. 对每个桶进行排序
+	//	for i:=0 ; i< len(bucketList) ; i++{
+	//		sort.Float64s(bucketList[i])
+	//	}
+	//5.输出全部元素
+	sortedArray := make([]float64, bucketNum)
+	index :=0
+	for _, list := range bucketList {
+		if list.Len() != 0 {
+			for _,element := range list {
+				sortedArray[index] = element
+				index++
+			}
+		}
+	}
+	fmt.Println(sortedArray)
+}
+
+type float64Heap []float64
+
+func (h float64Heap) Len() int           { return len(h) }
+func (h float64Heap) Less(i, j int) bool { return h[i] < h[j] }
+func (h float64Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *float64Heap) Push(x interface{}) {
+	*h = append(*h, x.(float64))
+}
+
+func (h *float64Heap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
